22c93e26a9ec2ba2d1ab73911ccb3fc06cdea106
Ayothedev <ayodma@gmail.com>
2020-10-11 23:04:25 -0500

    Update load_data.py

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -1,3 +1,13 @@
 import os
 import glob
 import csv
+
+sensor_data = load_sensor_data()
+sensor_files = glob.glob(os.path.join(os.getcwd(),'datasets','csv'))
+
+for sensor_files(sensor_file){
+with = open(sensor_file,'r') as data_file
+
+data_reader = csv.DictReader(data_file(delimiter=',',[]))
+
+}
